8c24eb0194c0071a6dfbb1462fa54f561a80c4f2
Eric <mateieric66@gmail.com>
2025-06-05 12:05:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,126 @@
-// Auto-scroll all image sliders
-document.querySelectorAll('.image-slider').forEach(slider => {
-  let scrollAmount = 0;
-  const speed = 2; // Increased speed from 1 to 2
-
-  setInterval(() => {
-    scrollAmount += speed;
-    if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
-      scrollAmount = 0;
-    }
-    slider.scrollLeft = scrollAmount;
-  }, 30); // Fast scroll loop
-});
-// Back to Top button logic
-const topBtn = document.getElementById("topBtn");
-
-window.onscroll = () => {
-  topBtn.style.display = window.scrollY > 200 ? "block" : "none";
-};
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
+const cars = [
+  {
+    name: "Toyota Prado TX",
+    price: 4500,
+    images: [
+      "https://toyota.tsusho.co.ke/images/newcars/prado/prado.png",
+      "https://toyotakenya.com/wp-content/uploads/2020/01/Toyota-Prado.jpg"
+    ],
+    details: "7-seater | Diesel | Automatic"
+  },
+  {
+    name: "Nissan Note",
+    price: 2000,
+    images: [
+      "https://cdn.motor1.com/images/mgl/BEpPx/s1/nissan-note.jpg",
+      "https://i.ytimg.com/vi/owR0epctTbg/maxresdefault.jpg"
+    ],
+    details: "5-seater | Petrol | Automatic"
+  },
+  {
+    name: "Mazda Demio",
+    price: 1800,
+    images: [
+      "https://www.autopax.net/uploads/Mazda%20Demio.jpg",
+      "https://www.japanesecartrade.com/assets/uploads/cars/mazda/demio/mazda-demio-1.jpg"
+    ],
+    details: "5-seater | Petrol | Auto/Manual Options"
+  },
+  {
+    name: "Toyota Noah",
+    price: 3500,
+    images: [
+      "https://toyota.jp/pages/contents/noah_top/images/noah_top_keyvisual_pc.jpg",
+      "https://www.autopax.net/uploads/Toyota%20Noah.jpg"
+    ],
+    details: "8-seater | Petrol | Family Van"
+  },
+  {
+    name: "Subaru Forester",
+    price: 4000,
+    images: [
+      "https://subaru.co.ke/wp-content/uploads/2023/07/forester_banner.jpg",
+      "https://www.subaru.com/content/dam/subaru/vehicles/forester/2022/overview/2022-forester-compact-suv-101.jpg"
+    ],
+    details: "5-seater | AWD | Turbo"
+  },
+  {
+    name: "Toyota Hilux Double Cab",
+    price: 5000,
+    images: [
+      "https://toyota.tsusho.co.ke/images/newcars/hilux/Hilux%20DC.png",
+      "https://toyotakenya.com/wp-content/uploads/2020/01/Toyota-Hilux-DC.jpg"
+    ],
+    details: "5-seater | Diesel | 4WD Pickup"
+  },
+  {
+    name: "Toyota Vitz",
+    price: 1700,
+    images: [
+      "https://carfromjapan.com/assets/img/blog/toyota-vitz-vs-yaris.jpg",
+      "https://www.beforward.jp/files/cars/TOYOTA/VITZ/2016/209958/BF722568_7e9824.jpg"
+    ],
+    details: "5-seater | Petrol | Compact City Car"
+  },
+  {
+    name: "Honda Fit",
+    price: 1800,
+    images: [
+      "https://hondakenya.com/wp-content/uploads/2020/07/Fit-1.jpg",
+      "https://hondakenya.com/wp-content/uploads/2020/07/Fit-2.jpg"
+    ],
+    details: "5-seater | Petrol | Automatic"
+  },
+  {
+    name: "Mitsubishi Outlander",
+    price: 4000,
+    images: [
+      "https://mitsubishi-motors.co.ke/wp-content/uploads/2020/01/Outlander-1.jpg",
+      "https://mitsubishi-motors.co.ke/wp-content/uploads/2020/01/Outlander-2.jpg"
+    ],
+    details: "7-seater | Petrol | AWD"
+  },
+  {
+    name: "Suzuki Swift",
+    price: 1600,
+    images: [
+      "https://suzuki.co.ke/wp-content/uploads/2020/01/Swift-1.jpg",
+      "https://suzuki.co.ke/wp-content/uploads/2020/01/Swift-2.jpg"
+    ],
+    details: "5-seater | Petrol | Compact"
+  },
+  {
+    name: "Ford Ranger",
+    price: 4800,
+    images: [
+      "https://ford.co.ke/wp-content/uploads/2020/01/Ranger-1.jpg",
+      "https://ford.co.ke/wp-content/uploads/2020/01/Ranger-2.jpg"
+    ],
+    details: "5-seater | Diesel | 4WD Pickup"
+  },
+  {
+    name: "Hyundai Tucson",
+    price: 4200,
+    images: [
+      "https://hyundai.co.ke/wp-content/uploads/2020/01/Tucson-1.jpg",
+      "https://hyundai.co.ke/wp-content/uploads/2020/01/Tucson-2.jpg"
+    ],
+    details: "5-seater | Petrol | SUV"
+  },
+  {
+    name: "Kia Sportage",
+    price: 4100,
+    images: [
+      "https://kia.co.ke/wp-content/uploads/2020/01/Sportage-1.jpg",
+      "https://kia.co.ke/wp-content/uploads/2020/01/Sportage-2.jpg"
+    ],
+    details: "5-seater | Petrol | SUV"
+  },
+  {
+    name: "Nissan X-Trail",
+    price: 4300,
+    images: [
+      "https://nissan.co.ke/wp-content/uploads/2020/01/X-Trail-1.jpg",
+      "https://nissan.co.ke/wp-content/uploads/2020/01/X-Trail-2.jpg"
+    ],
+    details:16
